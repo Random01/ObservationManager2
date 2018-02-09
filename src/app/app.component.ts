84986b96f64b560d77bde255248111bfd5216ac1
Alexsei Matvienko <alexsei.matvienko@thomsonreuters.com>
2018-02-09 09:45:27 +0400

    use html instead of string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-    <ul>
-        <li><a routerLink="/equipment">Equipment</a></li>
-        <li><a routerLink="/sites">Sites</a></li>
-        <li><a routerLink="/observations">Observations</a></li>
-    </ul>
-    <router-outlet></router-outlet>
-    `,
+  templateUrl: './app.component.html'
 })
 
 export class AppComponent  {
