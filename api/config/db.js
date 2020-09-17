5ce5361fbdb342f5e23056ea0d34f74e76b8db58
Random01 <random01@rambler.ru>
2020-09-17 10:09:50 +0400

    change

--- api/config/db.js
+++ api/config/db.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = {
-    url: 'mongodb+srv://db-user:prfKU5H3KIwEOYGs@om.haz76.azure.mongodb.net/om-1?retryWrites=true&w=majority'
+    url: 'mongodb+srv://db-user:<password>@om.haz76.azure.mongodb.net/om-1?retryWrites=true&w=majority'
 };
