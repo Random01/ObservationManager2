93f1a5dd7a19ad9511d6bbb7f274f94d1dd503b3
Random01 <random01@rambler.ru>
2025-11-04 08:20:32 +0400

    feat: remove odd constructor parameters

--- src/app/eyepieces/shared/eyepiece.service.ts
+++ src/app/eyepieces/shared/eyepiece.service.ts
@@ -1,13 +1,11 @@
 ﻿import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 import { Eyepiece } from '../../shared/models/equipment/eyepiece.model';
 import { StorageService } from '../../shared/services/storage.service';
-import { JwtService } from '../../auth/shared/jwt.service';
 
 @Injectable({ providedIn: 'root' })
 export class EyepieceService extends StorageService<Eyepiece> {
-  constructor(http: HttpClient, jwtService: JwtService) {
-    super('/eyepieces', http, jwtService, Eyepiece);
+  constructor() {
+    super('/eyepieces', Eyepiece);
   }
 }
--- src/app/scopes/shared/scope.service.ts
+++ src/app/scopes/shared/scope.service.ts
@@ -1,13 +1,11 @@
 ﻿import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 import { Scope } from '../../shared/models/equipment/scope.model';
 import { StorageService } from '../../shared/services/storage.service';
-import { JwtService } from '../../auth/shared/jwt.service';
 
 @Injectable({ providedIn: 'root' })
 export class ScopeService extends StorageService<Scope> {
-  constructor(http: HttpClient, jwtService: JwtService) {
-    super('/scopes', http, jwtService, Scope);
+  constructor() {
+    super('/scopes', Scope);
   }
 }
